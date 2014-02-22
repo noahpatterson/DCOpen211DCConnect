81795df5991414cef6ed792a8b6ba3ff94549bb8
Andrew Lomax <Lomax@Muse-2.local>
2014-02-22 16:40:45 -0500

    heroku

--- config/initializers/ohanapi.rb
+++ config/initializers/ohanapi.rb
@@ -4,7 +4,7 @@ stack = Faraday::Builder.new do |builder|
   builder.adapter Faraday.default_adapter
 end
 Ohanakapa.configure do |config|
-  config.api_token = ENV["OHANA_API_TOKEN"]
-  config.api_endpoint = ENV["OHANA_API_ENDPOINT"] || "http://ohanapi.herokuapp.com/api"
+  #config.api_token = ENV["OHANA_API_TOKEN"]
+  config.api_endpoint = "http://quiet-springs-1693.herokuapp.com/api"
   config.middleware = stack
 end
