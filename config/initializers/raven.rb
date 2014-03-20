ea72a10a87787a6aa04457b9d5394a07eb63759f
noahpatterson <n@noahpatterson.me>
2014-03-19 23:18:36 -0400

    add sentry api.Bundled

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,5 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.dsn = ''
+  config.dsn = 'https://3de9506285484cc6a9c8e28d59e532f5:de9dec0ea14d41ccbeaefb55461b8bbc@app.getsentry.com/21224'
 end
